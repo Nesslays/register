eb49f8b41791d21584b1edb6d885350ac8f709d2
Nelson <nelly.mutua@gmail.com>
2023-08-07 16:29:11 +0300

    h2

--- test.php
+++ test.php
@@ -96,7 +96,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>User Registration</title>
 </head>
 <body>
-    <h2>Sign-up</h2>
+    <h2>Sign-up Form</h2>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <label for="firstname">First Name:</label>
         <input type="text" name="firstname" required><br>
