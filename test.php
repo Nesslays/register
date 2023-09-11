223956b19f26109ef1fbe7fef751a4b07ebfb86b
Nelson <nelly.mutua@gmail.com>
2023-09-11 08:52:55 +0300

    inputadd

--- test.php
+++ test.php
@@ -117,7 +117,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="password" name="confirm_password" required><br>
 
         <input type="submit" value="Register">
-        <input type="reset" value="Clear">
+        <input type="reset" value="Reset">
+        
         <hr>
         
     </form>
