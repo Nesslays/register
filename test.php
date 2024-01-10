b04cb27f1c760ffbc63220790e829fad1c09cb21
Nelson <nelly.mutua@gmail.com>
2024-01-10 08:20:02 +0300

    t

--- test.php
+++ test.php
@@ -93,7 +93,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>User Registration Details</title>
+    <title>User Registration</title>
 </head>
 <body>
     <h2>Sign-up</h2>
