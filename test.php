08bbb55ba597f9acb53d390a6b187894ee1dacbf
Nelson <nelly.mutua@gmail.com>
2024-04-13 13:27:19 +0300

    links

--- test.php
+++ test.php
@@ -124,8 +124,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
         <hr>
         <a href="Login.php">Login to the System.
-        <a href="Reset-passsword.php"> Password Reset: 
-        <a href="google.com">use Google account: 
+       
            
           
       
