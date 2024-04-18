5247ef6b0be3e1599a8be1b148bb57ff7938c404
Nelson <nelly.mutua@gmail.com>
2024-04-18 09:00:30 +0300

    input field

--- test.php
+++ test.php
@@ -104,16 +104,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <label for="lastname">Last Name:</label>
         <input type="text" name="lastname" required><br>
 
-        <label for="user name">Last Name:</label>
+        <label for="user name">user Name:</label>
         <input type="text" name="username" required><br>
 
-        <label for="email">User Email:</label>
+        <label for="email">Email:</label>
         <input type="email" name="email" required><br>
 
         <label for="phone">PhoneNumber:</label>
         <input type="text" name="phone" required><br>
 
-        <label for="password">user Password:</label>
+        <label for="password">Password:</label>
         <input type="password" name="password" required><br>
 
         <label for="confirm_password"> Cornfarm Password:</label>
