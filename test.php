28a9e593d6ec5b1fe065b7715efc88d78425b343
Nelson <nelly.mutua@gmail.com>
2024-07-24 15:51:58 +0300

    href

--- test.php
+++ test.php
@@ -129,7 +129,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="reset" value="Reset">
         
         <hr>
-      
+      <a href="google.com"> google </a>
      
        
            
